209333e234fe91af7450c6aeb7bd0523fb0f7135
Ivan Pashkov <ivan_pashkov_99@mail.ru>
2024-01-03 02:27:04 +0300

    move auth provider

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useMemo } from 'react';
 import { Button, Navbar, Container } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
@@ -11,35 +10,24 @@ import {
   useLocation,
 } from 'react-router-dom';
 
-import AuthContext from '../contexts/index.js';
+import AuthProvider from '../contexts/AuthProvider.jsx';
 import useAuth from '../hooks/auth.js';
 import routes from '../routes.js';
+
 import Signup from './SignupPage.jsx';
 import LoginPage from './LoginPage.jsx';
 import ChatPage from './ChatPage.jsx';
 import NotFound from './NotFound.jsx';
 
-const AuthProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const logIn = () => setLoggedIn(true);
-  const logOut = () => {
-    localStorage.removeItem('userdata');
-    setLoggedIn(false);
-  };
-  const memoAuth = useMemo(() => ({ loggedIn, logIn, logOut }), [loggedIn]);
-  return (
-    <AuthContext.Provider value={memoAuth}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
   const location = useLocation();
+
   return auth.loggedIn
     ? children
-    : <Navigate to="/login" state={{ from: location }} />;
+    : <Navigate to={routes.loginPagePath()} state={{ from: location }} />;
 };
+
 const AuthButton = () => {
   const auth = useAuth();
   const { t } = useTranslation();
@@ -53,14 +41,14 @@ const App = () => {
     <AuthProvider>
       <div className="d-flex flex-column h-100">
         <Router>
+
           <Navbar expand="lg" variant="light" bg="white" className="shadow-sm">
             <Container>
-              <Navbar.Brand as={Link} to="/">{t('name')}</Navbar.Brand>
+              <Navbar.Brand as={Link} to={routes.chatPagePath()}>{t('name')}</Navbar.Brand>
               <AuthButton />
             </Container>
           </Navbar>
           <ToastContainer />
-
           <Routes>
             <Route
               path={routes.chatPagePath()}

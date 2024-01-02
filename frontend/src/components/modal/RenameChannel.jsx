b1d7ab1da088d9ba3f8e92d6e2c3f5fc4a65798d
Ivan Pashkov <ivan_pashkov_99@mail.ru>
2024-01-03 02:32:09 +0300

    refactoring modal input

--- frontend/src/components/modal/RenameChannel.jsx
+++ frontend/src/components/modal/RenameChannel.jsx
@@ -1,10 +1,9 @@
 import { toast } from 'react-toastify';
-import React, { useRef, useEffect } from 'react';
-import { Modal, Form, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
 
+import ModalInput from './ModalInput.jsx';
 import useSocket from '../../hooks/socket.js';
 import { closeModal } from '../../slices/modalSlice';
 import { newChannelSchema } from '../../validation/validationSchema';
@@ -13,22 +12,13 @@ const RenameChannel = () => {
   const { t } = useTranslation();
   const { renameChannel } = useSocket();
   const dispatch = useDispatch();
-  const inputModal = useRef(null);
-
-  useEffect(() => {
-    inputModal.current.focus();
-  }, []);
-
   const { channels } = useSelector((state) => state.channels);
   const { modals } = useSelector((state) => state.modals);
   const { isShown, targetId } = modals;
   const currentChanel = channels.find((el) => el.id === targetId);
-
   const formik = useFormik({
     initialValues: { channelName: currentChanel.name },
-
     validationSchema: newChannelSchema(channels, t('modal.unique'), t('modal.lengthParams')),
-
     onSubmit: (values) => {
       renameChannel({ name: values.channelName, id: targetId });
       dispatch(closeModal());
@@ -38,36 +28,16 @@ const RenameChannel = () => {
 
   const handleClose = () => dispatch(closeModal());
 
-  return (
-    <Modal show={isShown} centered>
-      <Modal.Header closeButton onHide={handleClose}>
-        <Modal.Title>{t('modal.rename')}</Modal.Title>
-      </Modal.Header>
+  const values = {
+    isShown,
+    handleClose,
+    title: t('modal.rename'),
+    formik,
+    cancelButton: t('cancel'),
+    submitButton: t('send'),
+  };
 
-      <Modal.Body>
-        <Form onSubmit={formik.handleSubmit}>
-          <Form.Label className="visually-hidden" htmlFor="channelName">{t('modal.rename')}</Form.Label>
-          <Form.Control
-            id="channelName"
-            name="channelName"
-            className="mb-2"
-            onChange={formik.handleChange}
-            value={formik.values.channelName}
-            isInvalid={formik.errors.channelName && formik.touched.channelName}
-            disabled={formik.isSubmitting}
-            ref={inputModal}
-          />
-          <Form.Control.Feedback type="invalid">{formik.errors.channelName}</Form.Control.Feedback>
-
-          <div className="d-flex justify-content-end">
-            <Button onClick={handleClose} variant="secondary" className="me-2">{t('cancel')}</Button>
-            <Button type="submit" variant="primary">{t('send')}</Button>
-          </div>
-
-        </Form>
-      </Modal.Body>
-    </Modal>
-  );
+  return <ModalInput values={values} />;
 };
 
 export default RenameChannel;
